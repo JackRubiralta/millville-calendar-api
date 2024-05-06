04ddf810f909dce6e7ff30602f5e2354bedd9ff6
Jack Rubiralta <jackrubiralta@outlook.com>
2024-05-06 19:51:34 -0400

    f

--- api.js
+++ api.js
@@ -208,8 +208,8 @@ const addEvents = async (events, calendarId) => {
     const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
     // max is 10 per second
-    const BATCH_SIZE = 9; // Maximum number of events per batch
-    const REQUEST_INTERVAL_MS = 1000; // Wait 1 second between batches
+    const BATCH_SIZE = 12; // Maximum number of events per batch
+    const REQUEST_INTERVAL_MS = 400; // Wait 1 second between batches
     let promises = [];
 
     for (let i = 0; i < events.length; i += BATCH_SIZE) {
